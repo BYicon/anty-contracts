c37ffccc5a2492634e0f9c91c3b85f8d7ae88f47
ioby <mt130827@163.com>
2024-12-11 22:03:18 +0800

    chore: remove unuse file

--- contracts/MIR.sol
+++ contracts/MIR.sol
@@ -7,32 +7,10 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract MIR is ERC20, Ownable {
     uint8 private constant _decimals = 6;
 
-    uint256 private rewardMir = 10 * 10 ** _decimals;
-
-    mapping(address => uint32) public lastDailyCheck;
-
-    event DailyCheck(address indexed user, uint256 indexed amount);
-
     constructor(
         uint256 initialSupply
     ) ERC20("MirrorToken", "MIR") Ownable(msg.sender) {
         _mint(msg.sender, initialSupply);
-    }
-
-    function dailyCheck() external {
-        require(msg.sender.code.length == 0, "Only user address");
-        require(
-            lastDailyCheck[msg.sender] == 0 ||
-                block.timestamp - lastDailyCheck[msg.sender] >= 1 days,
-            "Daily Check Error"
-        );
-        _mint(msg.sender, rewardMir);
-        lastDailyCheck[msg.sender] = uint32(block.timestamp);
-        emit DailyCheck(msg.sender, rewardMir);
-    }
-
-    function setRewardRate(uint256 _newRewardMir) external onlyOwner {
-        rewardMir = _newRewardMir;
     }
 
     function decimals() public view virtual override returns (uint8) {
--- deploy/01-deploy-mir-token.ts
+++ deploy/01-deploy-mir-token.ts
@@ -1,6 +1,10 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
 async function deployMirToken({getNamedAccounts, deployments}: HardhatRuntimeEnvironment) {
     const owner = (await getNamedAccounts()).owner369;
     console.log("owner 🚀🚀🚀" , owner);
+
+    // TODO: Deploy the MIR token
 };
 
 export default deployMirToken;
